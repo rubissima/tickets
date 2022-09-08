b65724cf271968efb374e035110de9d3d581324b
rubissima <rubykane@gmail.com>
2022-09-07 22:05:34 -0700

    dates

--- src/ResultCard.js
+++ src/ResultCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
 const ResultCard = ({event}) => {
+
     console.log(event);
     return (
         <div className="card w-96 bg-base-100 shadow-xl image-full">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <figure><img src={event.images[0].url} alt={event.name} /></figure>
             <div className="card-body">
                 <h2 className="card-title">{event.name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>{event._embedded.venues[0].name}</p>
+                <p>{moment(event.dates.start.dateTime).format("dddd, MMM DD, YYYY [at] h:mm a")}</p>
                 <div className="card-actions justify-end">
                 <button className="btn btn-primary">Buy Now</button>
                 </div>
